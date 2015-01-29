4efbdc075896fba4b77f05b2bdefccca985d5ee1
Puggan Se <from_git@puggan.se>
2015-01-29 22:51:03 +0100

    Made a class of plugin, insted of lose functions

--- owncloud_protection/owncloud_protection.php
+++ owncloud_protection/owncloud_protection.php
@@ -14,44 +14,59 @@ Author URI: http://blog.puggan.se
 
 DEFINE("OWNCLOUD_PROTECTION_PLUGIN_VERSION", '0.0.1');
 
-	function oc_init()
+class oc_protect
+{
+	public $settings = NULL;
+	public $oc_cookie_name = NULL;
+	public $user_id = NULL;
+	public $groups = NULL;
+	public $error = NULL;
+	
+	function __construct()
 	{
 		global $wpdb;
 
+		/// TODO: load settings
+		
+		// Fake settings
+		{
+			$this->settings['global block'] = TRUE;
+			$this->settings['oc url'] = "/";
+		}
+		
 		$old_session = session_id();
-		$oc_session = NULL;
 
 		foreach(array_keys($_COOKIE) AS $cookie_key)
 		{
 			if(substr($cookie_key, 0, 2) == "oc")
 			{
-				$oc_session = $cookie_key;
+				$this->oc_cookie_name = $cookie_key;
 				break;
 			}
 		}
 
-		if(!$oc_session)
+		if(!$this->oc_cookie_name)
 		{
-			/// TODO: get settings for login-page-url
-			header("Location: /");
-			die("No oc-cookie found");
+			$this->error = "No oc-cookie found";
+			return;
 		}
 
 		if($old_session)
 		{
 			session_write_close();
 		}
-		session_id($_COOKIE[$oc_session]);
+		session_id($_COOKIE[$this->oc_cookie_name]);
 		session_start();
 
-		if(!isset($_SESSION['user_id']) OR !$_SESSION['user_id'])
+		if(isset($_SESSION['user_id']) AND $_SESSION['user_id'])
 		{
-			/// TODO: get settings for login-page-url
-			header("Location: /");
-			die("No user_id found");
+			$this->user_id = $_SESSION['user_id'];
+		}
+		else
+		{
+			$this->error = "No user_id found";
 		}
 
-		$oc_user_id = $_SESSION['user_id'];
 		session_write_close();
 
 		if($old_session)
@@ -59,11 +74,40 @@ DEFINE("OWNCLOUD_PROTECTION_PLUGIN_VERSION", '0.0.1');
 			session_id($old_session);
 			session_start();
 		}
+		
+		if($this->error)
+		{
+			return;
+		}
 
 		$oc_groups = $wpdb->get_col($wpdb->prepare("SELECT gid FROM oc_group_user WHERE uid = %s", $oc_user_id));
-		$oc_groups = array_combine($oc_groups, $oc_groups);
-
-		$GLOBALS['oc'] = array('user_id' => $oc_user_id, 'groups' => $oc_groups);
+		$this->groups = array_combine($oc_groups, $oc_groups);
 	}
+	
+	public function init()
+	{
+		add_action('init', array($this, 'global_block_test'));
+	}
+	
+	public function global_block_test()
+	{
+		if(!$this->user_id AND $this->settings['global block'])
+		{
+			if($this->settings['oc url'])
+			{
+				header("Location: {$this->settings['oc url']}");
+				wp_die("Permission denied, global block for non owncloud users. " . $this->error, "Permission denied", array("response" => 307));
+				die();
+			}
+			else
+			{
+				wp_die("Permission denied, global block for non owncloud users. " . $this->error, "Permission denied", array("response" => 403));
+				die();
+			}
+		}
+	}
+}
 
-	oc_init();
+	$GLOBALS['oc_protect'] = new oc_protect();
+	
+	$GLOBALS['oc_protect']->init();
