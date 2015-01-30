640a6051c5334f96e0987ef25887e0ff576086ce
Puggan Se <from_git@puggan.se>
2015-01-31 00:45:17 +0100

    make wordpress use owncloud auth

--- owncloud_protection/owncloud_protection.php
+++ owncloud_protection/owncloud_protection.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @package Owncloud Protection
- * @author Puggan
- * @version 0.0.1-20150128
- */
+* @package Owncloud Protection
+* @author Puggan
+* @version 0.0.1-20150128
+*/
 /*
 Plugin Name: Owncloud Protection
 Description: Use owncloud as permission check for pages
@@ -11,6 +11,8 @@ Version: 0.0.1-20150128
 Author: Puggan
 Author URI: http://blog.puggan.se
 */
+
+// TODO: check if there actions/filters in current_user_can
 
 DEFINE("OWNCLOUD_PROTECTION_PLUGIN_VERSION", '0.0.1');
 
@@ -21,20 +23,21 @@ class oc_protect
 	public $user_id = NULL;
 	public $groups = NULL;
 	public $error = NULL;
-	
+
 	function __construct()
 	{
 		global $wpdb;
 
 		/// TODO: load settings
-		
+
 		// Fake settings
 		{
 			$this->settings['global block'] = get_option("oc_protect_global_block", TRUE);
-			$this->settings['oc url']  = get_option("oc_protect_url", "/");
+			$this->settings['oc_url'] = get_option("oc_protect_url", "/");
+			$this->settings['login_oc_url'] = get_option("oc_protect_login_url", "/");
 		}
-		
-		$old_session = session_id();
+
+		$old_session_id = session_id();
 
 		foreach(array_keys($_COOKIE) AS $cookie_key)
 		{
@@ -51,11 +54,12 @@ class oc_protect
 			return;
 		}
 
-		if($old_session)
+		if($old_session_id)
 		{
 			session_write_close();
 		}
 		session_id($_COOKIE[$this->oc_cookie_name]);
+		$old_session_name = session_name($this->oc_cookie_name);
 		session_start();
 
 		if(isset($_SESSION['user_id']) AND $_SESSION['user_id'])
@@ -69,12 +73,12 @@ class oc_protect
 
 		session_write_close();
 
-		if($old_session)
+		if($old_session_id)
 		{
-			session_id($old_session);
-			session_start();
+			session_id($old_session_id);
+			session_start($old_session_name);
 		}
-		
+
 		if($this->error)
 		{
 			return;
@@ -84,20 +88,106 @@ class oc_protect
 
 		$this->groups = array_combine($oc_groups, $oc_groups);
 	}
-	
+
+	public function check_permission($target)
+	{
+		if(!$this->user_id) return FALSE;
+
+		if(substr($target, 0, 1) == '@')
+		{
+			return (substr($target, 1) == $this->user_id);
+		}
+
+		return isset($this->groups[$target]);
+	}
+
+	public function check_permission_list($targets)
+	{
+		if(!$this->user_id) return FALSE;
+
+		if(!is_array($targets))
+		{
+			$targets = explode(" ", str_replace(",", " ", $permission[$ptype]));
+		}
+
+		foreach($targets as $current_target)
+		{
+			$current_target = trim($current_target);
+			if(!$current_target) continue;
+
+			if($this->check_permission($current_target))
+			{
+				return TRUE;
+			}
+		}
+
+		return FALSE;
+	}
+
 	public function init()
 	{
+		add_filter('determine_current_user', array($this, 'set_current_user'), 30);
+		add_action('clear_auth_cookie', array($this, 'clear_cookie'));
+		add_action('wp_logout', array($this, 'logout'));
 		add_action('init', array($this, 'global_block_test'));
 		add_action('widgets_init', array($this, 'register_widgets'));
+		add_action('add_meta_boxes_page', array($this, 'register_meta_box'));
+		add_action('save_post', array($this, 'save_page_permissions'));
 	}
-	
+
+	public function set_current_user($a = NULL, $b = NULL, $c = NULL)
+	{
+		$wanted_user = get_user_by('login', $this->user_id);
+
+		if(!$wanted_user OR !is_a($wanted_user, 'WP_User'))
+		{
+			$wanted_user = wp_insert_user(array('user_login' => $this->user_id, 'role' => 'editor'));
+
+			if(!$wanted_user OR !is_a($wanted_user, 'WP_User'))
+			{
+				return FALSE;
+			}
+		}
+
+		if($wanted_user->ID != $a)
+		{
+			wp_set_auth_cookie($wanted_user->ID, TRUE, TRUE);
+		}
+
+		return $wanted_user->ID;
+	}
+
+	public function clear_cookie()
+	{
+		if(session_id())
+		{
+			session_write_close();
+		}
+		session_id($_COOKIE[$this->oc_cookie_name]);
+		$old_session_name = session_name($this->oc_cookie_name);
+		session_start();
+		session_destroy();
+		setcookie($this->oc_cookie_name, "", time()-3600);
+	}
+
+	public function logout()
+	{
+		if($this->settings['login_oc_url'])
+		{
+			header("Location: {$this->settings['login_oc_url']}");
+			wp_die("Logged out from owncloud.", "Logged out", array("response" => 307));
+		}
+		wp_die("Logged out from owncloud", "Logged out", array("response" => 200));
+		return TRUE;
+	}
+
 	public function global_block_test()
 	{
 		if(!$this->user_id AND $this->settings['global block'])
 		{
-			if($this->settings['oc url'])
+			if($this->settings['login_oc_url'])
 			{
-				header("Location: {$this->settings['oc url']}");
+				header("Location: {$this->settings['login_oc_url']}");
 				wp_die("Permission denied, global block for non owncloud users. " . $this->error, "Permission denied", array("response" => 307));
 				die();
 			}
@@ -108,10 +198,55 @@ class oc_protect
 			}
 		}
 	}
-	
+
 	public function register_widgets()
 	{
 		register_widget( 'Owncloud_User_Status_Widget' );
+	}
+
+	public function register_meta_box()
+	{
+		add_meta_box("oc_page_permission", "Owncloud permissions", array($this, "meta_box_page_edit"), 'page', 'side');
+	}
+
+	public function meta_box_page_edit($current_post)
+	{
+		// TODO read about get_post_meta + update_post_meta
+		$read_permission = get_post_meta($current_post->ID, '_oc_read_permission', TRUE);
+		$edit_permission = get_post_meta($current_post->ID, '_oc_edit_permission', TRUE);
+
+		// TODO read about wp_nonce_field + wp_verify_nonce + sanitize_text_field
+		wp_nonce_field( 'oc_permission', 'oc_permission_nonce' );
+
+		echo "<div>";
+		echo '<label for="oc_read_permission">Read permission:</label> ';
+		echo '<input type="text" id="oc_read_permission" name="oc_read_permission" value="' . esc_attr( $read_permission ) . '" />';
+		echo "</div>";
+
+		echo "<div>";
+		echo '<label for="oc_edit_permission">Edit permission:</label> ';
+		echo '<input type="text" id="oc_edit_permission" name="oc_edit_permission" value="' . esc_attr( $edit_permission ) . '" />';
+		echo "</div>";
+	}
+
+	public function save_page_permissions($page_id)
+	{
+		// check for posible errors and verify that its a real permitted save page
+		if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
+		if(!isset($_POST['oc_permission_nonce'])) return;
+		if(!isset($_POST['oc_read_permission'])) return;
+		if(!isset($_POST['oc_edit_permission'])) return;
+		if(!wp_verify_nonce($_POST['oc_permission_nonce'], 'oc_permission')) return;
+		if(isset($_POST['post_type']) && 'page' != $_POST['post_type']) return;
+		if(!current_user_can('edit_page', $page_id)) return;
+
+		// Sanitize user input.
+		$read_permission = sanitize_text_field($_POST['oc_read_permission']);
+		$edit_permission = sanitize_text_field($_POST['oc_edit_permission']);
+
+		// Update the meta field in the database.
+		update_post_meta($page_id, '_oc_read_permission', $read_permission);
+		update_post_meta($page_id, '_oc_edit_permission', $edit_permission);
 	}
 }
 
@@ -122,7 +257,7 @@ class Owncloud_User_Status_Widget extends WP_Widget
 		parent::__construct(
 			'owncloud_user_status_widget', // Base ID
 			'Owncloud user status', // Name
-			array( 'description' => 'Show status for loged in owncloud user', ) // Args
+			array('description' => 'Show status for loged in owncloud user', ) // Args
 		);
 	}
 
@@ -136,15 +271,15 @@ class Owncloud_User_Status_Widget extends WP_Widget
 
 		echo $widget_args['before_widget'];
 
-		echo "<p><b>Owncloud user</b></p>";
-		
+		echo "{$widget_args['before_title']}Owncloud user{$widget_args['after_title']}";
+
 		if($GLOBALS['oc_protect']->user_id)
 		{
 			if($show_username)
 			{
 				echo "<p>User: {$GLOBALS['oc_protect']->user_id}</p>";
 			}
-			
+
 			if($show_groups)
 			{
 				if($GLOBALS['oc_protect']->groups)
@@ -156,26 +291,26 @@ class Owncloud_User_Status_Widget extends WP_Widget
 					echo "<p>Groups: (none)</p>";
 				}
 			}
-			
-			if($show_link AND $GLOBALS['oc_protect']->settings['oc url'])
+
+			if($show_link AND $GLOBALS['oc_protect']->settings['oc_url'])
 			{
-				echo "<p><a href='{$GLOBALS['oc_protect']->settings['oc url']}'>Show filelist</a></p>";
+				echo "<p><a href='{$GLOBALS['oc_protect']->settings['oc_url']}'>Show filelist</a></p>";
 			}
 		}
 		else
 		{
 			echo "<p>Not logged in</p>";
-			
-			if($GLOBALS['oc_protect']->settings['oc url'])
+
+			if($GLOBALS['oc_protect']->settings['oc_url'])
 			{
-				echo "<p><a href='{$GLOBALS['oc_protect']->settings['oc url']}'>Login</a></p>";
+				echo "<p><a href='{$GLOBALS['oc_protect']->settings['oc_url']}'>Login</a></p>";
 			}
 		}
-		
+
 		echo $widget_args['after_widget'];
 	}
 
- 	public function form( $instance )
+	public function form( $instance )
 	{
 		$fields = array();
 		$fields['username']['id'] = $this->get_field_id('show_username');
@@ -215,6 +350,8 @@ HTML;
 		return $instance;
 	}
 }
+
+	if(!ob_get_level()) ob_start();
 
 	$GLOBALS['oc_protect'] = new oc_protect();
 	$GLOBALS['oc_protect']->init();
